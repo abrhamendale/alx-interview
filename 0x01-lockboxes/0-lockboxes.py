9974b6f7d4559449b3f3d656ea425311483dc8e4
Abrham Endale <tmailae2020@gmail.com>
2023-04-28 00:34:02 +0300

    Fixed output errors.

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -31,7 +31,7 @@ def canUnlockAll(boxes):
     if len(boxes):
         keys[0] = 1
     else:
-        return (False)
+        return (True)
     for i in boxes[0]:
         if i < len(boxes):
             if keys[i] == 0:
