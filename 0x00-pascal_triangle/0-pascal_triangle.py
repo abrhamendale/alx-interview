20275e19de2a70f43c419dd4a483fa1614c1cbb8
Abrham Endale <tmailae2020@gmail.com>
2023-04-29 00:52:02 +0300

    Added a README and pascal triangle file.

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Pascal's triangle"""
+
+
+def pascal_triangle(n = 5):
+    """Returns pascals triangle in a list of lists format."""
+    tr = []
+    for i in range(n):
+        tr.append(list())
+        tr[i].append(1)
+        if i > 1:
+            for j in range(1, i):
+                tr[i].append(tr[i - 1][j] + tr[i - 1][j - 1])
+        if i > 0:
+            tr[i].append(1)
+    return (tr)
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,1 @@
+Python implementation of pascal's triangle.
