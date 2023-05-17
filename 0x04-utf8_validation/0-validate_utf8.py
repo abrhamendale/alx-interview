8a24c7e04a689bf629f262e7af5915175cba6e5c
Abrham Endale <abrhamendale9@gmail.com>
2023-05-17 07:19:17 +0000

    Added a README file.

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+UTF-8 validation module
+"""
+
+def validUTF8(data):
+    """Checks if an input is a valid UTF-8 encoding."""
+    for i in range(0, len(data)):
+        if int(data[i]) > 255:
+            return (False)
+    return (True)
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,1 @@
+A python code to check if a number is a valid UTF-8 encoding.
