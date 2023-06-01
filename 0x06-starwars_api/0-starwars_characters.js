b1f1f3a95b6bed3689973dd454583a6ca8a0476b
Abrham Endale <abrhamendale9@gmail.com>
2023-06-01 17:59:20 +0000

    A fix for starwars.

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,36 +1,24 @@
 #!/usr/bin/node
-//Star wars
+//fetches Star wars characters of a movie
 
 const request = require('request');
 
-function fetch_char(url_char) {
-  var option_a = {
-	  url: url_char,
-	  method: 'GET'
-  }
-  var n;
-  request(option_a, function(err, res, body) {
-    let results = JSON.parse(body);
-    n = results.name;
-  });
-  return n;
+var option_a = {
+  url: "https://swapi.dev/api/films/" + process.argv[2],
+  method: 'GET'
 }
-var option_b = {
-	url: "https://swapi.dev/api/films/" + process.argv[2],
-	method: 'GET'
-}
-function fetch_film(){
-  var name_list = [];
-  var d = []
-  request(option_b, function(err, res, body) { 
-    let data = JSON.parse(body);
-    d = data.characters;
-  });
-  for (let i = 0; i < d.length; i++) {
-    name_list.push(fetch_char(d[i]));
+request(option_a, function(err, res, body) {
+  let results = JSON.parse(body);
+  let nm = results.name;
+  if (nm) {
+    fetch_char(0, nm, nm.length);
   }
-  for (let i = 0; i < name_list.length; i++) {
-    console.log(name_list[i]);
+});
+function fetch_char(index, ch, len){
+  for(let i = 0, i < len, i++) {
+    request(ch[index], function(err, res, body) { 
+      let data = JSON.parse(body);
+      console.log(data.name);
+    });
   }
 }
-fetch_film();
