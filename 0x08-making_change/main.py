c1c433f4e49d2ad6539beb81a057432b7c731317
Abrham Endale <abrhamendale9@gmail.com>
2023-06-18 17:13:23 +0000

    A fix for empty coins.

--- 0x08-making_change/main.py
+++ 0x08-making_change/main.py
@@ -9,6 +9,6 @@ print(makeChange([125, 25, 2, 1, 5], 292))
 
 print(makeChange([1256, 54, 48, 16, 102], 1453))
 
-#print(makeChange([1256, 54, 48, 16, 102], -1476))
+print(makeChange([1256, 54, 48, 16, 102], 2882))
 
 #print(makeChange([100, 100, 100, 100, 100, 100, 100], 30079))
